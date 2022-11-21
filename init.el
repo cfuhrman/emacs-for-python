b75b6d8e7da9fa01616216a835bf5ec151b3912d
Christopher M. Fuhrman <christopher.fuhrman@shipwire.com>
2022-11-21 13:56:59 -0800

    Repress warnings from lsp-ui & helm-lsp

--- init.el
+++ init.el
@@ -52,7 +52,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(helm-themes helm-theme use-package helm doom-themes doom-modeline)))
+   '(lsp-ui helm-lsp helm-themes helm-theme use-package helm doom-themes doom-modeline))
+ '(warning-suppress-log-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
